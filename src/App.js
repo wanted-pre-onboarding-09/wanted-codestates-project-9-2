7df4bf926d929f41fd61fbe1718e88b2cb5a6334
youngjeJO <yj01591@gmail.com>
2022-03-07 18:14:48 +0900

    fix header layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import './App.css';
 import Home from './components/pages/Home';
 
@@ -7,6 +8,20 @@ function App() {
     <div className="App">
       <Home />
     </div>
+=======
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserPage from './components/pages/UserPage';
+import HomePage from './components/pages/HomePage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </BrowserRouter>
+>>>>>>> e1b4d702350747a9856e13960ed57ac7995a794c
   );
 }
 
