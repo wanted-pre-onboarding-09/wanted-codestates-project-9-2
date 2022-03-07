aa98a4c9b4ac20fab46db0120381fce96c7a9d1a
ysh2987 <ysh2987@naver.com>
2022-03-08 02:28:15 +0900

    #36 Modify: redux store

--- src/store/match/matchAsyncThunk.js
+++ src/store/match/matchAsyncThunk.js
@@ -6,7 +6,7 @@ export const getMatch = createAsyncThunk(
   async ({ nickName, gameType }) => {
     const getAccessId = await axios.get(`users/nickname/${nickName}`);
     const match = await axios.get(
-      `users/${getAccessId.data.accessId}/matches?start_date=&end_date= &offset=0&limit=10&match_types=${gameType}`,
+      `users/${getAccessId.data.accessId}/matches?start_date=&end_date= &offset=0&limit=200&match_types=${gameType}`,
     );
 
     return match.data;
