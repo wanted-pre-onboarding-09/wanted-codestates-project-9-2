680ff4e9eabe1d18811190489968928d0bd2e65b
zeromountain <sonyeongsan@gmail.com>
2022-03-07 15:24:11 +0900

    chore: project init

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'object-curly-newline': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'comma-dangle': 'off',
+    semi: 'off',
+    'space-before-function-paren': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        parser: 'flow',
+        endOfLine: 'auto',
+      },
+    ],
+    'multiline-ternary': 'off',
+    indent: 'off',
+    'linebreak-style': 0,
+    'no-param-reassign': 'off',
+    'no-shadow': 'off',
+    'consistent-return': 'off',
+    'jsx-a11y/img-redundant-alt': 'off',
+    'react/no-array-index-key': 'off',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'react/jsx-key': 'off',
+  },
+};
