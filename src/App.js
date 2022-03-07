96718e5a5653eb5c79078eaa3d812b6aafd590e5
y-solb <ysolb99@gmail.com>
2022-03-07 18:38:45 +0900

    Feat: Add RankPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserPage from './components/pages/UserPage';
 import HomePage from './components/pages/HomePage';
+import RankPage from './components/pages/RankPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/user" element={<UserPage />} />
+        <Route path="/rank" element={<RankPage />} />
       </Routes>
     </BrowserRouter>
   );
