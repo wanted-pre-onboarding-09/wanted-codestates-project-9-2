511c59a7457a63bbd52dcbf8bd5e42c96d5f30cd
y-solb <ysolb99@gmail.com>
2022-03-08 21:59:37 +0900

    Feat: Add GameTypeTab data

--- src/components/organisms/RankInfoContainer.js
+++ src/components/organisms/RankInfoContainer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import RankGuideButton from '../atoms/RankGuideButton';
-import SoloOrTeamTabBar from '../molecules/SoloOrTeamTabBar';
+import GameTypeTab from '../molecules/GameTypeTab';
 import ThreeTabBar from '../molecules/ThreeTabBar';
 import TopRank from '../molecules/TopRank';
 
-const RankInfoContainer = ({ topRank }) => (
+const RankInfoContainer = ({ topRank, gameType, handleGameType }) => (
   <InfoContainer>
     <RankTitle>3월 TMI 랭킹</RankTitle>
     <Line />
@@ -21,7 +21,7 @@ const RankInfoContainer = ({ topRank }) => (
     </RankInfo>
     <RankGuideButton />
     <Tabs>
-      <SoloOrTeamTabBar />
+      <GameTypeTab gameType={gameType} handleGameType={handleGameType} />
       <LineSpan>|</LineSpan>
       <ThreeTabBar />
     </Tabs>
